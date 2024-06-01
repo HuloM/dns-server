4feb0b47241dfaf843ca6bdc3e696d4770a88a47
matt <matthieu.l.dubray@gmail.com>
2024-05-31 21:51:25 -0700

    DNS parsing question section

--- app/dns/answer.py
+++ app/dns/answer.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from app.dns.record_data import RecordClass, RecordType
-
+import ipaddress
 
 @dataclass
 class Answer:
@@ -51,9 +51,7 @@ class Answer:
         self.answer += self.ttl.to_bytes(4, byteorder='big')
         self.answer += self.length.to_bytes(2, byteorder='big')
 
-        split_rdata = self.rdata.split('.')
-
-        for i in split_rdata:
-            self.answer += int(i).to_bytes(1, byteorder='big')
+        ip = ipaddress.ip_address(self.rdata)
+        self.answer += int(ip).to_bytes(4, byteorder='big')
 
         return self.answer
