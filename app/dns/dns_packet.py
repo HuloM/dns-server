1d53f62df345ed8904eebdf753d7d183e7e99db7
matt <matthieu.l.dubray@gmail.com>
2024-05-31 19:20:05 -0700

    DNS question section

--- app/dns/dns_packet.py
+++ app/dns/dns_packet.py
@@ -1,24 +1,29 @@
 from app.dns.header import Header, OpCode, RCode
+from app.dns.question import Question, RecordType, RecordClass
 
 
-def construct_dns():
+def construct_dns(query_url):
     dns_pkt = b''
 
     # preset values for testing
     dns_pkt += Header(
-        id=1234,
-        qr=True,
-        op_code=OpCode.QUERY,
-        aa=False,
-        tc=False,
-        rd=False,
-        ra=False,
-        z=0,
-        r_code=RCode.NO_ERROR,
-        qd_count=0,
-        an_count=0,
-        ns_count=0,
-        ar_count=0,
+        id       = 1234,
+        qr       = True,
+        op_code  = OpCode.QUERY,
+        aa       = False,
+        tc       = False,
+        rd       = False,
+        ra       = False,
+        z        = 0,
+        r_code   = RCode.NO_ERROR,
+        qd_count = 1,
+        an_count = 0,
+        ns_count = 0,
+        ar_count = 0,
     ).to_bytes()
+
+    dns_pkt += Question(record_type  = RecordType.A,
+                        record_class = RecordClass.IN
+                        ).name(query_url)
 
     return dns_pkt
--- app/main.py
+++ app/main.py
@@ -5,19 +5,22 @@ from app.dns import dns_packet
 def main():
     # Uncomment this block to pass the first stage
 
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind(("127.0.0.1", 2053))
+    response = dns_packet.construct_dns('codecrafters.io')
+    print(response)
 
-    while True:
-        try:
-            buf, source = udp_socket.recvfrom(512)
-
-            response = dns_packet.construct_dns()
-
-            udp_socket.sendto(response, source)
-        except Exception as e:
-            print(f"Error receiving data: {e}")
-            break
+    # udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # udp_socket.bind(("127.0.0.1", 2053))
+    #
+    # while True:
+    #     try:
+    #         buf, source = udp_socket.recvfrom(512)
+    #
+    #         response = dns_packet.construct_dns()
+    #
+    #         udp_socket.sendto(response, source)
+    #     except Exception as e:
+    #         print(f"Error receiving data: {e}")
+    #         break
 
 
 if __name__ == "__main__":
